b592e58bef3fca769fdc5205d71eb4525d9c5806
nazlihan867 <nazlihan-atan@web.de>
2025-02-10 18:22:11 +0100

    video embedded

--- frontend/src/pages/Landingpage.jsx
+++ frontend/src/pages/Landingpage.jsx
@@ -3,7 +3,17 @@ import Layout from "../layout/layout";
 function HomePage() {
   return (
     <Layout>
-     <h1>Test</h1> 
+      <div>
+        <iframe
+          className="w-full min-h-screen"
+          src="https://www.youtube.com/embed/aj1Xj-WN8uo?si=UHPSDRxEEQSvAmNE"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
     </Layout>
   );
 }
