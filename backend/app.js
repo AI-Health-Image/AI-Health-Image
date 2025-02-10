40aa19676edcc7f9d4a0d91f474e3b391ddbcc15
Zerrelius <Jeremy.Held@web.de>
2025-02-10 15:47:00 +0100

    Added Prisma and sqlite Database, Login Route, Register Route

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+// Common JS module
+const express = require('express');
+const cors = require('cors');
+const authRouter = require("./routes/auth");
+
+// Initialisierung von Express
+const app = express();
+const port = process.env.PORT || 3000;
+
+// CORS Optionen (Cross-Origin Resource Sharing)
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+};
+
+// Middleware für Form Data
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Initialisieren der Grund Route
+app.get("/", (request, response) => {
+    response.send("Nothing to see - go away!");
+});
+
+// Auth Route
+app.use("/auth", authRouter);
+
+// Server wird gestartet auf die Variable Port
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
