e646def5f3e0585df278e7aa1065e35c85fe5780
nazlihan867 <nazlihan-atan@web.de>
2025-02-11 12:40:33 +0100

    added picture and text

--- frontend/src/pages/Landingpage.jsx
+++ frontend/src/pages/Landingpage.jsx
@@ -14,6 +14,11 @@ function HomePage() {
           allowFullScreen
         ></iframe>
       </div>
+
+      <div className="flex justify-between items-center gap-2">
+        <img src="werbung.jpg" alt="werbungsblock" className="w-20" ></img>
+        <p className=" bg-amber-600 p-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      </div>
     </Layout>
   );
 }
