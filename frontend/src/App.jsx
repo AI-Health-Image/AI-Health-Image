1fc2de4aa0ea4da88874cf0a29126e9c4598dd60
masterkey24 <manuel.huck@edu.techstarter.de>
2025-02-12 11:14:49 +0100

    rename schema.env in .env; modified app.jsx, create a upload_button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import Landingpage from "./pages/Landingpage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
-import ImprintPage from "./pages/ImprintPage.jsx"
+import ImprintPage from "./pages/ImprintPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import PrivacyPage from "./pages/PrivacyPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
-
+import UploadbuttonPage from "./pages/UploadbuttonPage.jsx";
 
 function App() {
-
   return (
     <>
-   
-  
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landingpage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/privacy" element={<PrivacyPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/imprint" element={<ImprintPage />}/>
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/imprint" element={<ImprintPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/upload" element={<UploadbuttonPage />} />
+        </Routes>
+      </BrowserRouter>
 
-
-<h1>Hallo World</h1>
+      <h1>Hallo World</h1>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
