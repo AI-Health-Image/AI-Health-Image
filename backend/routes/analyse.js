a144caa533b6d2fbf8c974014d3a26cf179936d5
Zerrelius <Jeremy.Held@web.de>
2025-02-20 20:21:32 +0100

    Fixed Analysis Page, changed get function from backend, added a datatable in the DB

--- backend/routes/analyse.js
+++ backend/routes/analyse.js
@@ -1,34 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
-const jwt = require('jsonwebtoken');
 const prisma = new PrismaClient();
 const path = require('path');
 const { spawn } = require('child_process');
-const fs = require('fs');
 
 router.get('/api/image/:id', async (req, res) => {
-    const { id } = req.params;
-    const archiveId = parseInt(id);
-    console.log(id);
-    const files = await prisma.userUploads.findMany({
-        where: {
-            archivId: archiveId,
-        },
-    }); 
-    console.log(files);
-   
-
-   res.json({ data: files });
-});
-
-// Separate route for serving files
-router.get('/uploads/:filename', (req, res) => {
-    const filePath = path.resolve('./data/upload', req.params.filename);
-    res.sendFile(filePath);
-});
-
-router.get('/api/analyse/:id', async (req, res) => {
     const { id } = req.params;
     const archiveId = parseInt(id);
     const files = await prisma.userUploads.findMany({
@@ -36,32 +13,110 @@ router.get('/api/analyse/:id', async (req, res) => {
             archivId: archiveId,
         },
     });
-    console.log(files);
+    res.json({ data: files });
+});
 
-    const brain = spawn('../ki-modell/venv/scripts/python', ['../ki-modell/brain.py', files[0].uploadedFilname]);
-    console.log(brain);
-    const result = await new Promise((resolve, reject) => {
-        brain.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-        });
+router.get('/uploads/:id', async (req, res) => {
+    const imageId = parseInt(req.params.id);
+    const imageData = await prisma.userUploads.findUnique({
+        where: {
+            id: imageId,
+        },
+    });
+    const filePath = path.resolve('./data/upload', imageData.uploadedFilname);
+    res.sendFile(filePath);
+});
+
+router.get('/api/analyse/:filename', async (req, res) => {
+    const filename = req.params.filename;
+    //console.log('Filename:', filename);
+    const file = await prisma.userUploads.findFirst({
+        where: {
+            uploadedFilname: filename,
+        },
+    });
+    //console.log(file);
+
+    const user = await prisma.users.findUnique({
+        where: {
+            id: file.userId,
+        },
+    });
+    //console.log('User:', user);
+
+    const brain = spawn('../ki-modell/venv/scripts/python', ['../ki-modell/brain.py', file.uploadedFilname]);
+    let resultData = '';
+
+    brain.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        resultData += data.toString();
+    });
+
+    await new Promise((resolve, reject) => {
         brain.on('exit', (code) => {
             console.log(`child process exited with code ${code}`);
             resolve(code);
         });
     });
-    console.log(result);
+    //console.log(resultData);
 
-    res.json({ data: files });
+    const date = new Date();
+    //console.log('archiveId:', file.archivId, 'Uploaded Filename:', file.uploadedFilname, 'User Id:', user.id, 'User Email:', user.email, 'Result:', resultData, 'Date:', date);
+    await prisma.analysedUserUploads.create({
+        data: {
+            uploadedFilname: file.uploadedFilname,
+            userId: user.id,
+            analysed: true,
+            result: resultData,
+            date: date,
+            archivId: file.archivId,
+        },
+    });
+
+    res.json({ data: file.uploadedFilname });
 });
 
-// Separate route for serving files
-router.get('/output/:filename', (req, res) => {
-    const filePath = path.resolve('./data/output', req.params.filename);
-    console.log(filePath);
-    if (!fs.existsSync(filePath)) {
-        return res.status(201).send('File not found');
+router.get('/output/:filename', async (req, res) => {
+    const fileName = req.params.filename;
+    //console.log('output filename:', fileName);
+    const imageData = await prisma.analysedUserUploads.findFirst({
+        where: {
+            uploadedFilname: fileName,
+        },
+    });
+    //console.log('ImageData:', imageData);
+    if (!imageData) {
+        return res.status(204).send('No database entry found');
     }
+    const filePath = path.resolve('./data/output', imageData.uploadedFilname);
+    //console.log(filePath);
+    /*
+    if (!fs.existsSync(filePath)) {
+        return res.status(204).send('File not found');
+    }*/
+    //console.log('File sended');
     res.sendFile(filePath);
+});
+
+router.get('/output/verify/:filename', async (req, res) => {
+    const fileName = req.params.filename;
+    //console.log('Filename:', fileName);
+    const verifiedImage = await prisma.analysedUserUploads.findFirst({
+        where: {
+            uploadedFilname: fileName,
+        },
+    });
+    //console.log('verifiedImage:', verifiedImage);
+    if (!verifiedImage) {
+        return res.status(204).send('No database entry found');
+    }
+    if (verifiedImage.result === '0') {
+        return res.status(204).send('No result found');
+    }
+
+    const filePath = (verifiedImage.uploadedFilname);
+
+    res.send(filePath);
 });
 
 module.exports = router;
--- ki-modell/brain.py
+++ ki-modell/brain.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 import torch.nn as nn
 from torchvision import transforms
@@ -5,9 +6,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import time
 from thop import profile
-import sys
 import os
-import pathlib as Path
 
 class SimpleCNN(nn.Module):
     def __init__(self, model_type='c', num_classes=6): # The ‘model_type’ variable can be changed to ‘f, c, q’ according to the model type.
@@ -138,6 +137,8 @@ def main():
         print(f'Speed CPU b1 (ms): {metrics["speed_cpu_b1"]:.2f} ms')
         print(f'Speed V100 b1 (ms): {metrics["speed_v100_b1"]:.2f} ms')
         print(f'Speed V100 b32 (ms): {metrics["speed_v100_b32"]:.2f} ms')
+        print(f'Prediction: {class_names[predicted_class]}')
+        print(f'Accuracy: {confidence:.2f}%')
 
         output_dir = '../backend/data/output/'
         if not os.path.exists(output_dir):
