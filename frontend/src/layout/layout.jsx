bc4561974113009ba46f1efad36cd719b889240d
C.Erdi64 <cansin.erdi@gmail.com>
2025-02-25 00:41:15 +0100

    applied changes for Aboutpage

--- frontend/src/layout/layout.jsx
+++ frontend/src/layout/layout.jsx
@@ -1,13 +1,14 @@
 import Header from "../components/Layout_Coms/Header";
 import Footer from "../components/Layout_Coms/Footer";
 
-
 function Layout({ children }) {
   return (
-    <div className="flex flex-col h-screen justify-between gap-2">
-      <Header className="h-10" />
-      {children}
-      <Footer className="h-10" />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow relative z-10">
+        {children}
+      </main>
+      <Footer />
     </div>
   );
 }
