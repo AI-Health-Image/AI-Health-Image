ba12eaee1bab6eccd55f94a7c044e870b964c619
Zerrelius <Jeremy.Held@web.de>
2025-03-06 15:32:07 +0100

    try to fix husky

--- frontend/src/tests/imprintPage.test.jsx
+++ frontend/src/tests/imprintPage.test.jsx
@@ -10,7 +10,7 @@ describe('Imprint Page', () => {
         render(<BrowserRouter><ImprintPage /></BrowserRouter>);
         const div = screen.getByTestId('div1');
         expect(div).toBeInTheDocument();
-        expect(div).toHaveClass('bg-cyan-200');
+        expect(div).toHaveClass('flex');
     });
 
     test('h2', () => {
