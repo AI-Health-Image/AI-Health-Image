76838a5a3584f56170b863eedaef05ae452ac6fd
masterkey24 <manuel.huck@edu.techstarter.de>
2025-03-13 14:34:24 +0100

    added proptypes-library, then imported prop-types in layout.jsx, and modified layout.jsx (defined prop-types for layout)

--- frontend/src/layout/layout.jsx
+++ frontend/src/layout/layout.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import Header from "../components/Layout_Coms/Header";
 import Footer from "../components/Layout_Coms/Footer";
 
@@ -12,5 +13,9 @@ function Layout({ children }) {
     </div>
   );
 }
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Layout;
